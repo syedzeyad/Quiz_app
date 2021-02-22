b135782c5e4f313b2ca044c914142dd04d576bb1
Syed Zeyad <46657958+syedzeyad@users.noreply.github.com>
2021-02-22 21:51:04 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Questionnaire from './components/Questionnaire'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>JavaScript Quiz</h1>
+  <Questionnaire />
+    </div>
+  );
+}
+
+export default App;
