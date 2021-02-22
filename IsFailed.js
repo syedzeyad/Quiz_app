51a94727be4238744806cbd02eb49c104cb7479a
Syed Zeyad <46657958+syedzeyad@users.noreply.github.com>
2021-02-22 21:37:45 +0500

    Create IsFailed.js

---
+++ IsFailed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IsFailed() {
+    return (
+        <div>
+            <h2 className="IsFailed">You are Failed</h2>
+        </div>
+    )
+}
+
+export default IsFailed;
