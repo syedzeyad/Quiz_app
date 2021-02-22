b1e6c4913c07d7b09423baac4e9ed33d34d925d3
Syed Zeyad <46657958+syedzeyad@users.noreply.github.com>
2021-02-22 21:38:37 +0500

    Create IsPassed.js

---
+++ IsPassed.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function IsPassed() {
+    return (
+        <div>
+            <h2 className="IsPassed">You are Passed!</h2>
+        </div>
+    )
+}
+
+export default IsPassed;
+  
