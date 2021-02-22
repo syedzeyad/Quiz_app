8b0215172f6cf19a6f12ed95889877fbca126978
Syed Zeyad <46657958+syedzeyad@users.noreply.github.com>
2021-02-22 21:48:09 +0500

    Create TotalIncorrect.js

---
+++ TotalIncorrect.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TotalIncorrect(props) {
+    return (
+        <h3 className="correct">Incorrect: {props.incorrect}</h3> 
+    )
+}
+
+export default TotalIncorrect;
